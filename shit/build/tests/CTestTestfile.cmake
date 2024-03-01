ac0c0835ad8de78b48bce599cf7261cb4b3fd292
SmirnovaSofia <146405563+SmirnovaSofia@users.noreply.github.com>
2024-03-01 15:08:46 +0700

    Delete shit/build directory

--- shit/build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Professional/Desktop/МОП/Sugar_daddy/shit/tests
-# Build directory: C:/Users/Professional/Desktop/МОП/Sugar_daddy/shit/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("C:/Users/Professional/Desktop/МОП/Sugar_daddy/shit/build/tests/hello_test2[1]_include.cmake")
-subdirs("../_deps/googletest-build")
--- shit/build/tests/hello_test2[1]_tests.cmake
+++
@@ -1,3 +0,0 @@
-add_test([=[MyClassTest.test_sozd]=]  [==[C:/Users/Professional/Desktop/МОП/Sugar_daddy/shit/build/tests/Debug/hello_test2.exe]==] [==[--gtest_filter=MyClassTest.test_sozd]==] --gtest_also_run_disabled_tests)
-set_tests_properties([=[MyClassTest.test_sozd]=]  PROPERTIES WORKING_DIRECTORY [==[C:/Users/Professional/Desktop/МОП/Sugar_daddy/shit/build/tests]==] SKIP_REGULAR_EXPRESSION [==[\[  SKIPPED \]]==])
-set(  hello_test2_TESTS MyClassTest.test_sozd)
